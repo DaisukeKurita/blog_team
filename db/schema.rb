d25e6635abe0e34b94b4f5bf4da8145e96739741
atsushi oshima <aboutagial@yahoo.co.jp>
2022-03-20 15:03:38 +0900

    User.Blogアソシエーション作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_20_052846) do
-
+ActiveRecord::Schema.define(version: 2022_03_20_055323) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_blogs_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -24,12 +33,5 @@ ActiveRecord::Schema.define(version: 2022_03_20_052846) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
-  create_table "blogs", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "image"
-  end
-
+  add_foreign_key "blogs", "users"
 end
